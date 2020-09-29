fb2bb3ade93b7ecf2f878f1f9a6824172bdde1ba
lixali <48850628+lixali@users.noreply.github.com>
2020-09-29 09:13:11 -0400

    update clocks for the score calculations; more clocks for score calculations added

--- FPGA/ultra96_random_walk/bram_random_walk.v
+++ FPGA/ultra96_random_walk/bram_random_walk.v
@@ -9,14 +9,14 @@ module bram_random_walk();
     // score_table_offset is determined by number of seeds nodes plus number of nodes in the dictionary * 2 plus nei_table size; The following number are just picked for testing purpose for now 
     parameter period = 10, ADDR_WIDTH = 13, DATA_WIDTH = 32, DEPTH = 8192, nei_addr_table_offset = 10, nei_table_offset = 100, score_table_offset = 1000;
     parameter alpha = 0.95, seed_num = 10, m_rw = 100, max_steps = 7, node_num = 100; // hyper parameters for the random walk
-    reg step_clk, rw_clk, seed_clk, write_enable_reg;
+    reg step_clk, rw_clk, seed_clk, score_nodenum_clk, score_timestep_clk,divide_degree_clk,score_node_operation_clk;
     reg [ADDR_WIDTH-1:0] address, z;
-    reg [DATA_WIDTH-1:0] steps, steps_s, curr_node, start_node, curr_counter, counter_randomness, curr_score_s, start_node_degree, self_degree;
-    reg [DATA_WIDTH-1:0] y, first_neighbour_address, last_neighbour_address, next_node;
+    reg [DATA_WIDTH-1:0] steps, steps_s, curr_node, start_node, curr_counter, counter_randomness, curr_score_s, curr_score_d, start_node_degree, curr_node_degree , self_degree;
+    reg [DATA_WIDTH-1:0] y, first_neighbour_address, last_neighbour_address, first_neighbour_address_d, last_neighbour_address_d, next_node;
     integer i, rw_count, seed_count, read_count;
-    integer get_degree_counter = 0; 
-    integer steps_count_s = 0;//integer score_table_size = 
-    reg [DATA_WIDTH-1:0] curr_node_s = 32'h00000001;
+    integer get_degree_counter = 0, score_iter = 0, operation_count_d=0, get_degree_score_d = 0; 
+    integer steps_count_s = 0, max_score_table_iter = (node_num+1)*(node_num+1)*max_steps*5;//integer score_table_size = 
+    reg [DATA_WIDTH-1:0] curr_node_s = 32'h00000001, curr_node_d = 32'h00000001;
     reg [DATA_WIDTH-1:0] start_node_s = 32'h00000001;
 
     always begin
@@ -25,19 +25,35 @@ module bram_random_walk();
     end
 
     always begin
-        #(period/2 * max_steps) step_clk = ~step_clk;  // timescale is 1ns so #5 provides 100MHz clock
+        #(period/2 * max_steps) step_clk = ~step_clk;  
         rw_count += 1;
         counter_randomness = counter_randomness + 1'b1;
 
     end
 
     always begin
-        #(period/2 * max_steps * m_rw) rw_clk = ~rw_clk;  // timescale is 1ns so #5 provides 100MHz clock
+        #(period/2 * max_steps * m_rw * 5) rw_clk = ~rw_clk;  // times 5 is because there are 5 operations in the counter update for each node
         counter_randomness = counter_randomness + 1'b1;
 
     end
 
-    always @(negedge rw_clk) begin // start at the negative edge
+    always begin // 5 operations (4 read and 1 write)
+        #(period/2 * 5) score_node_operation_clk = ~score_node_operation_clk;  // 
+    end   
+
+    always begin
+        #(period/2 *  node_num * 5) score_nodenum_clk = ~score_nodenum_clk;  // 
+    end   
+
+    always begin
+        #(period/2 *  node_num * max_steps * 5) score_timestep_clk = ~score_timestep_clk;  // 
+    end     
+
+    always begin // this clock is for dividing the node's own degree; there are 4 operations (3 read and 1 write) for each node 
+        #(period/2 *  4) divide_degree_clk = ~divide_degree_clk;  // each node has 4 operation (3 read and 1 write); therefore, curr_
+    end       
+
+    always @(posedge rw_clk) begin // double check on waveform whether it should start on pos or neg edge
         #(period) start_node = data_out;
         seed_count += 1;
     end 
@@ -47,6 +63,23 @@ module bram_random_walk();
         steps = steps + 1'b1;
     end 
 
+    always @(posedge score_node_operation_clk) begin 
+        start_node_s =  start_node_s + 32'h00000001;  
+    end    
+
+    always @(posedge score_nodenum_clk) begin 
+        start_node_s =  32'h00000001;  // node starts at 1; currently there is no 0 node yet
+        steps_count_s += 1;
+    end    
+
+    always @(posedge score_timestep_clk) begin 
+        steps_count_s =  0;
+        curr_node_s = curr_node_s + 1'b1;
+    end  
+
+    always @(posedge divide_degree_clk) begin 
+        curr_node_d = curr_node_d + 1'b1;
+    end  
 
     always @(negedge clk) begin // start at the negative edge
         if (seed_count < seed_num) begin
@@ -69,12 +102,12 @@ module bram_random_walk();
                 #(period) last_neighbour_address = data_out; // read out the address of curr_node's last neighbour            
             end else if (read_count == 3) begin
                 write_enable = 1'b0;
-                address = (counter_randomness % (last_neighbour_address-first_neighbour_address)) + first_neighbour_address; // randomly pick the one of neighbour's address
+                address = (counter_randomness % (last_neighbour_address-first_neighbour_address+1)) + first_neighbour_address; // randomly pick the one of neighbour's address
                 read_count += 1;
                 write_enable = ~write_enable;
                 #(period) next_node = data_out; // read out next node    
                 curr_node = next_node;       
-            end else if (read_count > 3) begin
+            end else if (read_count == 4) begin
                 write_enable = 1'b1;
                 read_count = 0; // reset
                 curr_counter += 1; // counter value +=1 
@@ -84,7 +117,7 @@ module bram_random_walk();
             end
         // the following loop will be calculating the score value and write into BRAM;
         // the _s naming in the following loop means that it is for calculating the score table (for the purpose of distinguish from the above variable naming)
-        end else if (seed_count >= seed_num )begin 
+        end else if (seed_count >= seed_num && score_iter <= max_score_table_iter)begin 
             if (curr_node_s <= node_num) begin
                 if (steps_count_s < max_steps) begin
                     if (start_node_s <= node_num) begin
@@ -102,11 +135,11 @@ module bram_random_walk();
                             get_degree_counter += 1;
                             address = curr_node_s*2 + 1'b1 + nei_addr_table_offset;
                             #(period) last_neighbour_address = data_out; // read out the address of curr_node's first neighbour
-                            start_node_degree = last_neighbour_address - first_neighbour_address;
+                            start_node_degree = last_neighbour_address - first_neighbour_address+1;
                         end else if (get_degree_counter == 2) begin
                             write_enable = 1'b0; 
                             get_degree_counter += 1;
-                            y = (start_node_s - 1) * max_steps + steps_s;   
+                            y = (start_node_s - 1'b1) * max_steps + steps_s;   
                             z = curr_node_s*(max_steps*node_num) + y + nei_table_offset;
                             address = z;
                             #(period) curr_counter = data_out; // read the counter value  
@@ -115,7 +148,7 @@ module bram_random_walk();
                             write_enable = 1'b0;
                             get_degree_counter += 1;
                             address = curr_node_s + score_table_offset;
-                            #(period) curr_score_s = data_out; // read out the address of curr_node's first neighbour
+                            #(period) curr_score_s = data_out; // read current node's score
 
                         end else if (get_degree_counter == 4) begin
                             get_degree_counter = 0; // reset to go back to 1st read opeation
@@ -123,13 +156,40 @@ module bram_random_walk();
                             curr_score_s = curr_score_s + (alpha ** steps_count_s)*curr_counter*start_node_degree;
                             data_in = curr_score_s; // write the updated score into BRAM
                         end  
-                        start_node_s = start_node_s + 1'b1;
+                        score_iter += 1;
                     end
-                    steps_count_s += 1;
                 end
-                curr_node_s = curr_node_s + 1'b1;
 
             end
+        // the following variable with _d (divided by node's own degree) are used to differential the above _s (score table) naming 
+        end else if(operation_count_d < node_num*4)begin // each node has 4 operattions (3 read and 1 write), thereore times 4
+            if (curr_node_d <= node_num) begin
+                if (get_degree_score_d == 0) begin 
+                    get_degree_score_d += 1;
+                    write_enable = 1'b0; 
+                    address = curr_node_d*2 + nei_addr_table_offset;
+                    #(period/2) first_neighbour_address_d = data_out;
+                end else if (get_degree_score_d == 1) begin
+                    get_degree_score_d += 1;
+                    write_enable = 1'b0; 
+                    address = curr_node_d*2+ 1'b1 + nei_addr_table_offset;
+                    #(period/2) last_neighbour_address_d = data_out;
+                    curr_node_degree = last_neighbour_address_d - first_neighbour_address_d + 1;
+                end else if (get_degree_score_d == 2) begin
+                    get_degree_score_d += 1;
+                    write_enable = 1'b0; 
+                    address = curr_node_d + score_table_offset;
+                    #(period/2) curr_score_d = data_out;                    
+                end else if (get_degree_score_d == 3) begin
+                    get_degree_score_d = 0; //reset back to 0 so that it will go back to read operation for the next node 
+                    write_enable = 1'b1; 
+                    address = curr_node_d + score_table_offset;
+                    curr_score_d = curr_score_d / curr_node_degree;
+                    data_in = curr_score_d;                    
+                end
+
+            end
+            operation_count_d += 1;
         end
     end
     
