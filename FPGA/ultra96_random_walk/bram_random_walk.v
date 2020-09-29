9c87c9efd9d5ae604781ec3bd55f32684e063499
lixali <48850628+lixali@users.noreply.github.com>
2020-09-29 05:58:25 -0400

    refactor code and fix syntax issue

--- FPGA/ultra96_random_walk/bram_random_walk.v
+++ FPGA/ultra96_random_walk/bram_random_walk.v
@@ -1,23 +1,23 @@
 `timescale 1ns / 1ps
 
-module bram_random_walk(
-    input reg clk, write_enable, 
-    input reg [DATA_WIDTH-1:0] data_in;
-    output reg [DATA_WIDTH-1:0] data_out;
-);
-    parameter ADDR_WIDTH = 13, DATA_WIDTH = 32, DEPTH = 8192, seed_offset = 10, nei_table_offset = 30, score_table_offset = 100;
+module bram_random_walk();
+
+    reg clk, write_enable;
+    reg [DATA_WIDTH-1:0] data_in;
+    reg reg [DATA_WIDTH-1:0] data_out;
+
+    parameter period = 10, ADDR_WIDTH = 13, DATA_WIDTH = 32, DEPTH = 8192, seed_offset = 10, nei_table_offset = 30, score_table_offset = 100;
     parameter alpha = 1, seed_num = 10, m_rw = 100, max_steps = 7, node_num = 100; // hyper parameters for the random walk
 
-    reg step_clk, rw_clk, seed_clk;
+    reg step_clk, rw_clk, seed_clk, write_enable_reg;
     reg [ADDR_WIDTH-1:0] address, z;
-    reg [DATA_WIDTH-1:0] steps, steps_s, curr_node, counter_randomness, curr_score_s, start_node_degree, self_degree;
-    wire [DATA_WIDTH-1:0] y;
+    reg [DATA_WIDTH-1:0] steps, steps_s, curr_node, start_node, curr_counter, counter_randomness, curr_score_s, start_node_degree, self_degree;
+    reg [DATA_WIDTH-1:0] y, first_neighbour_address, last_neighbour_address, next_node;
     integer i, rw_count, seed_count, read_count;
     integer get_degree_counter = 0; 
-    integer steps_count_s = 0;
-    integer score_table_size = 
-    reg [DATA_WIDTH-1:0] curr_node_s = 8'h00000001;
-    reg [DATA_WIDTH-1:0] start_node_s = 8'h00000001;
+    integer steps_count_s = 0; //integer score_table_size = 
+    reg [DATA_WIDTH-1:0] curr_node_s = 32'h00000001;
+    reg [DATA_WIDTH-1:0] start_node_s = 32'h00000001;
 
     always begin
         #(period/2) clk = ~clk;  // timescale is 1ns so #5 provides 100MHz clock
@@ -25,14 +25,14 @@ module bram_random_walk(
     end
 
     always begin
-        #(period/2 * max_steps) step_clk = ~step_clk;  // timescale is 1ns so #5 provides 100MHz clock
+        #(period/2 * max_steps) step_clk = ~step_clk;  
         rw_count += 1;
         counter_randomness = counter_randomness + 1'b1;
 
     end
 
     always begin
-        #(period/2 * max_steps * m_rw) rw_clk = ~rw_clk;  // timescale is 1ns so #5 provides 100MHz clock
+        #(period/2 * max_steps * m_rw) rw_clk = ~rw_clk;  
         counter_randomness = counter_randomness + 1'b1;
 
     end
@@ -50,28 +50,33 @@ module bram_random_walk(
 
     always @(negedge clk) begin // start at the negative edge
         if (seed_count < seed_num) begin
-            y = (start_node - 1) * max_steps + steps  // there is a read cycle here, data_out is the seed_ 
+            y = (start_node - 1) * max_steps + steps;  // there is a read cycle here, data_out is the seed_ 
             z = curr_node*(max_steps*node_num) + y + nei_table_offset; // double check if this adddress offset is correct?
-            if(write_enable == 0 and read_count == 0) begin
+            if(read_count == 0) begin
+                write_enable = 1'b0;
                 address = z;
                 read_count += 1;
                 #(period) curr_counter = data_out; // read the counter value    
-            end else if (write_enable == 0 and read_count == 1) begin
+            end else if (read_count == 1) begin
+                write_enable = 1'b0;
                 address = curr_node*2 + seed_offset;
                 read_count += 1;
                 #(period) first_neighbour_address = data_out; // read out the address of curr_node's first neighbour
-            end else if (write_enable == 0 and read_count == 2) begin
+            end else if (read_count == 2) begin
+                write_enable = 1'b0;
                 address = curr_node*2 + 1'b1 + seed_offset;
                 read_count += 1;
                 #(period) last_neighbour_address = data_out; // read out the address of curr_node's last neighbour            
-            end else if (write_enable == 0 and read_count == 3) begin
+            end else if (read_count == 3) begin
+                write_enable = 1'b0;
                 address = (counter_randomness % (last_neighbour_address-first_neighbour_address)) + first_neighbour_address; // randomly pick the one of neighbour's address
                 read_count += 1;
                 write_enable = ~write_enable;
                 #(period) next_node = data_out; // read out next node    
                 curr_node = next_node;       
-            end else if (write_enable == 1 or read_count > 3) begin
-                read_count = 0 // reset
+            end else if (read_count > 3) begin
+                write_enable = 1'b1;
+                read_count = 0; // reset
                 curr_counter += 1; // counter value +=1 
                 write_enable = 1'b1;
                 data_in = curr_counter; // write the counter value to BRAM
@@ -124,7 +129,8 @@ module bram_random_walk(
                 end
                 curr_node_s = curr_node_s + 1'b1;
 
-            
+            end
+        end
     end
     
     bram bram_test (
