6ec34b443fd63284abb444dd75adc0c9249c529d
lixali <48850628+lixali@users.noreply.github.com>
2020-10-25 17:04:57 -0400

    fix compare condition and get rid of negedge condition

--- FPGA/diffusion_rw/conflict_block.v
+++ FPGA/diffusion_rw/conflict_block.v
@@ -5,8 +5,9 @@ module conflict_block #(parameter ADDR_WIDTH = 13, lower_addr = 0, upper_addr = 
     output reg conflict //
     );
 
-    always @(negedge clk) begin // start at the negative edge
-        if ( lower_addr <= addrA <= upper_addr && lower_addr <= addrB <= upper_addr) begin
+
+    always @(*) begin 
+        if (addrA >= lower_addr && addrA <= upper_addr && addrB >= lower_addr && addrB <= upper_addr) begin
             conflict <= 1'b1;
         end else begin
             conflict <= 1'b0;
